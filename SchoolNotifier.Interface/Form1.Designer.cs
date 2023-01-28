1d914ed229b2350750470e2cc6fabb60be3f2bd5
bohdankonopolskyi <bohdan_konopolskyi@icloud.com>
2023-01-28 14:57:21 +0200

    Form1 refactor

---
+++ SchoolNotifier.Interface/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SchoolNotifier.Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectScheduleBtn = new System.Windows.Forms.Button();
+            this.scheduleFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.selectAudioFileBtn = new System.Windows.Forms.Button();
+            this.audioFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.activateBtn = new System.Windows.Forms.Button();
+            this.disableBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectScheduleBtn
+            // 
+            this.SelectScheduleBtn.Location = new System.Drawing.Point(125, 283);
+            this.SelectScheduleBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SelectScheduleBtn.Name = "SelectScheduleBtn";
+            this.SelectScheduleBtn.Size = new System.Drawing.Size(302, 39);
+            this.SelectScheduleBtn.TabIndex = 0;
+            this.SelectScheduleBtn.Text = "Обрати розклад";
+            this.SelectScheduleBtn.UseVisualStyleBackColor = true;
+            this.SelectScheduleBtn.Click += new System.EventHandler(this.SelectScheduleBtn_Click);
+            // 
+            // scheduleFilePathTextBox
+            // 
+            this.scheduleFilePathTextBox.Location = new System.Drawing.Point(33, 231);
+            this.scheduleFilePathTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.scheduleFilePathTextBox.Name = "scheduleFilePathTextBox";
+            this.scheduleFilePathTextBox.Size = new System.Drawing.Size(480, 23);
+            this.scheduleFilePathTextBox.TabIndex = 1;
+            // 
+            // selectAudioFileBtn
+            // 
+            this.selectAudioFileBtn.Location = new System.Drawing.Point(747, 294);
+            this.selectAudioFileBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.selectAudioFileBtn.Name = "selectAudioFileBtn";
+            this.selectAudioFileBtn.Size = new System.Drawing.Size(155, 27);
+            this.selectAudioFileBtn.TabIndex = 2;
+            this.selectAudioFileBtn.Text = "Обрати аудіо";
+            this.selectAudioFileBtn.UseVisualStyleBackColor = true;
+            this.selectAudioFileBtn.Click += new System.EventHandler(this.selectAudioFileBtn_Click);
+            // 
+            // audioFilePathTextBox
+            // 
+            this.audioFilePathTextBox.Location = new System.Drawing.Point(637, 231);
+            this.audioFilePathTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.audioFilePathTextBox.Name = "audioFilePathTextBox";
+            this.audioFilePathTextBox.Size = new System.Drawing.Size(431, 23);
+            this.audioFilePathTextBox.TabIndex = 3;
+            // 
+            // activateBtn
+            // 
+            this.activateBtn.Location = new System.Drawing.Point(43, 457);
+            this.activateBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.activateBtn.Name = "activateBtn";
+            this.activateBtn.Size = new System.Drawing.Size(88, 27);
+            this.activateBtn.TabIndex = 4;
+            this.activateBtn.Text = "Активувати";
+            this.activateBtn.UseVisualStyleBackColor = true;
+            this.activateBtn.Click += new System.EventHandler(this.activateBtn_Click);
+            // 
+            // disableBtn
+            // 
+            this.disableBtn.Location = new System.Drawing.Point(306, 459);
+            this.disableBtn.Name = "disableBtn";
+            this.disableBtn.Size = new System.Drawing.Size(96, 23);
+            this.disableBtn.TabIndex = 5;
+            this.disableBtn.Text = "Деактивувати";
+            this.disableBtn.UseVisualStyleBackColor = true;
+            this.disableBtn.Click += new System.EventHandler(this.disableBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 752);
+            this.Controls.Add(this.disableBtn);
+            this.Controls.Add(this.activateBtn);
+            this.Controls.Add(this.audioFilePathTextBox);
+            this.Controls.Add(this.selectAudioFileBtn);
+            this.Controls.Add(this.scheduleFilePathTextBox);
+            this.Controls.Add(this.SelectScheduleBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectScheduleBtn;
+        private System.Windows.Forms.TextBox scheduleFilePathTextBox;
+        private System.Windows.Forms.Button selectAudioFileBtn;
+        private System.Windows.Forms.TextBox audioFilePathTextBox;
+        private System.Windows.Forms.Button activateBtn;
+        private Button disableBtn;
+    }
+}
+
---
+++ SchoolNotifier.Interface/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using SchoolNotifier;
+
+namespace SchoolNotifier.Interface
+{
+    public partial class Form1 : Form
+    {
+        private string _scheduleFilePath = @"C:\Users\kadde\Desktop\times.txt";
+        private string _audioFilePath = @"C:\Users\kadde\Downloads\sample-5s.wav";
+        private DailyTriggerSetuper _setuper;
+        private IFileReader _reader;
+        public Form1()
+        {
+            InitializeComponent();
+           _reader = new ScheduleFileReader();
+            _setuper = new  DailyTriggerSetuper(_reader);
+        }
+
+        private void SelectScheduleBtn_Click(object sender, EventArgs e)
+        {
+            string filePath = OpenDialog("txt files (*.txt)|*.txt|All files (*.*)|*.*");
+
+            scheduleFilePathTextBox.Text = filePath;
+        }
+
+        private void selectAudioFileBtn_Click(object sender, EventArgs e)
+        {
+            string filePath = OpenDialog();
+            _audioFilePath = filePath;
+            audioFilePathTextBox.Text = filePath;
+        }
+
+        private string OpenDialog(string filter = "")
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (filter.Length != 0)
+            { openFileDialog.Filter = filter; }
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+
+            return string.Empty;
+        }
+
+        private void activateBtn_Click(object sender, EventArgs e)
+        {
+            var player = new SoundPlayer(_audioFilePath);
+
+            Action action = () => player.Play();
+
+            _setuper.SetUpIntervals(_scheduleFilePath);
+            _setuper.SubscribeTriggers(action);
+        }
+
+        private void disableBtn_Click(object sender, EventArgs e)
+        {
+            _setuper.UnsubscribeTriggers();
+        }
+    }
+}
